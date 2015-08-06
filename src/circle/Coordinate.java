8f795aa44e84208d3ea8d6dae3cd052d7dcd6efd
Gavin <Gavin>
2015-08-06 09:18:09 +0800

    圆坐标计算代码格式调整

--- src/circle/Coordinate.java
+++ src/circle/Coordinate.java
@@ -18,9 +18,10 @@ import static java.lang.Math.toRadians;
 public class Coordinate {
     /**
      * 计算圆上任意一点的坐标，根据圆心坐标，半径， 和X正轴的圆心夹角。
-     * @param x 圆心坐标x
-     * @param y 圆心坐标y
-     * @param r 圆半径
+     *
+     * @param x     圆心坐标x
+     * @param y     圆心坐标y
+     * @param r     圆半径
      * @param angle 与X正轴的夹角
      * @return 在圆上的端点坐标
      */
@@ -33,9 +34,10 @@ public class Coordinate {
     /**
      * 计算圆上任意一点的坐标，根据圆心坐标，半径， 和X正轴的圆心夹角。
      * 这个方法根据四舍五入返回整形的数值
-     * @param x 圆心坐标x
-     * @param y 圆心坐标y
-     * @param r 圆半径
+     *
+     * @param x     圆心坐标x
+     * @param y     圆心坐标y
+     * @param r     圆半径
      * @param angle 与X正轴的夹角
      * @return 在圆上的端点坐标
      */
@@ -47,18 +49,19 @@ public class Coordinate {
 
     /**
      * 判断一个坐标是否在给定圆的内部
+     *
      * @param circleX 圆心坐标X
      * @param circleY 圆心坐标Y
      * @param circleR 圆的半径
-     * @param x 给定点的坐标X
-     * @param y 给定点的坐标Y
+     * @param x       给定点的坐标X
+     * @param y       给定点的坐标Y
      * @return true 如果坐标落在圆的内部， 否则返回false
      */
-    public static boolean insideCircle(double circleX, double circleY, double circleR, double x, double y){
-        return Math.hypot(circleX - x, circleY -y) <= circleR;
+    public static boolean insideCircle(double circleX, double circleY, double circleR, double x, double y) {
+        return Math.hypot(circleX - x, circleY - y) <= circleR;
     }
 
-    public static void main(String...args){
+    public static void main(String... args) {
         System.out.println(Math.hypot(-2, 2));
     }
 }
